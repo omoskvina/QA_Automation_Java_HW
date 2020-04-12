a6b391d454d6f9c36a214ec59b49da2ce202ca46
omoskvina <moskwinaes@gmail.com>
2020-04-13 00:06:19 +0300

    + lesson 8

--- HomeworkLessonEight/HomeworkLesson8.java
+++ HomeworkLessonEight/HomeworkLesson8.java
@@ -1,4 +1,17 @@
 package HomeworkLessonEight;
 
 public class HomeworkLesson8 {
+
+    public static void main(String[] args) {
+        SolarSystem solarSystemObject = new SolarSystem("Gas giant", 1898, 14313, 69911, 4.503F);
+        Planet planet = new Planet("Jupiter", "H, He", "ice, vapor", "dipole, higher components", "gas, liquid, metallic H, gas He", 79, 1.326F, 3.13F, true);
+
+
+
+
+        solarSystemObject.move(254000);
+        planet.move(30000);
+        planet.radiate("x-rays");
+
+    }
 }
